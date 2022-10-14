83db3b28cd6095dd5077c44cb716df7fe52af501
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-14 21:23:37 +0300

    fix

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -9,141 +9,226 @@ const { Node } = require('../extensions/list-tree.js');
 
 
 class BinarySearchTree {
+
   constructor() {
+
     this.rootNodeElement = null;
+
   }
 
   root() {
-    return rootBST(this.rootNodeElement)
 
-    function rootBST(n) {
+    return rootFun(this.rootNodeElement)
+
+    function rootFun(n) {
+
       if (n) {
+
         return n
+
       } else {
+
         return null
+
       }
+
     }
+
   }
 
   add(data) {
-    this.rootNodeElement = addWithin(this.rootNodeElement, data);
-    function addWithin(node, data) {
+
+    this.rootNodeElement = addWith(this.rootNodeElement, data);
+
+    function addWith(node, data) {
+
       if (!node) {
+
         return new Node(data)
+
       }
 
       if (node.data === data) {
+
         return node;
+
       }
 
       if (data < node.data) {
-        node.left = addWithin(node.left, data)
+
+        node.left = addWith(node.left, data)
+
       } else {
-        node.right = addWithin(node.right, data)
+
+        node.right = addWith(node.right, data)
+
       }
+
       return node
+
     }
+
   }
 
   has(data) {
-    return searchWithin(this.rootNodeElement, data)
 
-    function searchWithin(node, data) {
+    return search(this.rootNodeElement, data)
+
+    function search(node, data) {
+
       if (!node) {
+
         return false;
+
       }
 
       if (node.data === data) {
+
         return true;
+
       }
 
-      return data < node.data ? searchWithin(node.left, data) : searchWithin(node.right, data)
+      return data < node.data ? search(node.left, data) : search(node.right, data)
+
     }
+
   }
 
   find(data) {
-    return findData(this.rootNodeElement, data)
 
-    function findData(node, data) {
+    return findDataNode(this.rootNodeElement, data)
+
+    function findDataNode(node, data) {
+
       if (node === null) {
+
         return null;
+
       } else if (data < node.data) {
-        return findData(node.left, data)
+
+        return findDataNode(node.left, data)
+
       } else if (data > node.data) {
-        return findData(node.right, data)
+
+        return findDataNode(node.right, data)
+
       } else {
+
         return node
+
       }
+
     }
+
   }
 
   remove(data) {
-    this.rootNodeElement = removeNode(this.rootNodeElement, data);
 
-    function removeNode(node, data) {
+    this.rootNodeElement = removeNodeElem(this.rootNodeElement, data);
+
+    function removeNodeElem(node, data) {
+
       if (!node) {
+
         return null;
+
       }
 
       if (data < node.data) {
-        node.left = removeNode(node.left, data)
+
+        node.left = removeNodeElem(node.left, data)
+
         return node;
+
       } else if (data > node.data) {
-        node.right = removeNode(node.right, data)
+
+        node.right = removeNodeElem(node.right, data)
+
         return node
+
       } else {
+
         if (!node.left && !node.right) {
+
           return null
+
         }
 
         if (!node.left) {
+
           node = node.right
+
           return node
+
         }
 
         if (!node.right) {
+
           node = node.left
+
           return node
+
         }
 
-        let minFromRight = node.right;
-        while (minFromRight.left) {
-          minFromRight = minFromRight.left;
-        }
-        node.data = minFromRight.data
+        let minToRight = node.right;
 
-        node.right = removeNode(node.right, minFromRight.data);
+        while (minToRight.left) {
+
+          minToRight = minToRight.left;
+
+        }
+
+        node.data = minToRight.data
+
+        node.right = removeNodeElem(node.right, minToRight.data);
 
         return node
+
       }
+
     }
+
   }
 
   min() {
+
     if (!this.rootNodeElement) {
+
       return;
+
     }
 
-    let node = this.rootNodeElement;
-    while (node.left) {
-      node = node.left
+    let nodeElem = this.rootNodeElement;
+
+    while (nodeElem.left) {
+
+      nodeElem = nodeElem.left
+
     }
 
-    return node.data
+    return nodeElem.data
   }
 
   max() {
+
     if (!this.rootNodeElement) {
+
       return;
+
     }
 
     let node = this.rootNodeElement;
+
     while (node.right) {
+
       node = node.right
+
     }
 
     return node.data
+
   }
+
 }
 
 module.exports = {
